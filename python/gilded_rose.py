0c971cf598954506b32fe9d450454a79fb15f430
Roniel Gomes <roniel_ramos@outlook.com>
2021-06-07 22:52:33 -0300

    refactor: init and call use case class for update quality

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,27 @@
 # -*- coding: utf-8 -*-
 
+from support import QualityUpdater, SellInUpdater
+from use_cases import UpdateQualityOnItem
+
+
 class GildedRose(object):
+
+    LEGENDARY_ITEM_NAMES = ['Sulfuras, Hand of Ragnaros']
 
     def __init__(self, items):
         self.items = items
+        self.quality_updater = QualityUpdater()
+        self.sell_in_updater = SellInUpdater()
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            update_quality_on_item = UpdateQualityOnItem(
+                item,
+                self.sell_in_updater,
+                self.quality_updater,
+                self.LEGENDARY_ITEM_NAMES
+            )
+            update_quality_on_item.execute()
 
 
 class Item:
